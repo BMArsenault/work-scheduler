1f6ccaf87b23594befe89c391ed03176599a584c
Brian <BMArsenault@gmail.com>
2021-11-05 11:59:24 -0700

    updated html and script.js

--- script.js
+++ script.js
@@ -4,35 +4,38 @@ $("#currentDay").html(dateToday);
 
 function updateTimeSlotItems() {
 
-    // apply hours through Moment.js (.time-block class) and compare with current time
-    var now = moment().hour();
+    // // apply hours through Moment.js (.hour class) and compare with current time
+    // var now = moment().hour();
 
-    $(".time-block").each(function () {
-        var timeSlot = 
+    // $(".hour").each(function () {
+    //     var timeSlot = 
 
-    // less than hours means past
-         if (timeSlot < now) {
-
-
-       }  // equal to 0 is present
-       else if (timeSlot === now) {
+    // // less than hours means past
+    //      if (timeSlot < now) {
 
 
-       }
-         // greater than hours is future
-        else { (timeSlot > now) 
+    //    }  // equal to 0 is present
+    //    else if (timeSlot === now) {
 
-        }
-    })
+
+    //    }
+    //    // greater than hours is future
+    //     else { (timeSlot > now) 
+
+    //     }
+    // })
 
 }
 
 function updatePlanner() {
 
 // save button 'click'
-    // get value of description
-// var text =
-    // get value of time-block
+//    // get value of description
+// var text = 
+//    // get value of time-block
 // var time =
-    // store date in time-block with description
+//    // store date in time-block with description
+// localstorage.setItem(time, text);
 }
+
+updateTimeSlotItems();
