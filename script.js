a955338d818315321bb3b916bc94fe27d2cb4b81
Brian <BMArsenault@gmail.com>
2021-11-06 08:18:10 -0700

    added data-hour class

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
 // //  display date/time to top of page
-// var dateToday = moment().format("MMMM Do, YYYY");
-// $("#currentDay").append(dateToday);
+var dateToday = moment().format("MMMM Do, YYYY");
+$("#currentDay").append("Today is " + dateToday);
 
 // // // get current time in hours
-// var now = moment().hour();
+var currentHour = moment().hour();
+console.log("currenthour:",currentHour);
+var textarea = [$(".description").text()]
 
-// // set up save button
-// var saveBtn = $('.saveBtn')
-// saveBtn.click(handleSave)
+
 
     function updateTimeSlotItems() {
 
         // grab hour integer from HTML ID for moment.js
         $(".time-block").each(function() {
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            var timeBlock = parseInt($(this).attr("data-hour"));
+            console.log(timeBlock);
 
-            // compare "now" with timeBlock above
-            // if time has passed the block, set as "past"
-            if (now.isAfter(timeBlock))
-            {   
-                $(".description").addClass("past");
-            }
+            // compare current with timeBlock above
             //  if time is on current block of time, set as "present"
-            else if (now === timeBlock)
-            {
-                $(".description").addClass("present");
+            if (currentHour === timeBlock)
+            {   
+                $(this).addClass("present");
             }
             //  if time hasn't reached block of time, set as "future"
-            else (now < timeBlock)
+            else if (currentHour < timeBlock)
             {
-                $('.description').addClass("future");
+                $(this).addClass("future");
+            }
+             // if time has passed the block, set as "past"
+            else 
+            // (currentHour > timeBlock)
+            {
+                $(this).addClass("past");
             }
         }
         );
     };
 
+updateTimeSlotItems();
+
 // function updatePlanner() {
 
-// // save button (.saveBtn) 'click'
+// // set up save button
+// var saveBtn = $('.saveBtn')
+// saveBtn.click(handleSave)
 // //    // get value of description
 // // var text = 
 // //    // get value of time-block
