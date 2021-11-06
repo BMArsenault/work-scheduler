5e0e3a220777d273d25dd1bc6d1634366baaa70a
Brian <BMArsenault@gmail.com>
2021-11-05 21:39:21 -0700

    added parseInt from HTML ID

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
 //  display date/time to top of page
-var dateToday = moment().format('LLLL');
-$("#currentDay").html(dateToday);
+var dateToday = moment().format("MMMM Do, YYYY");
+$("#currentDay").append(dateToday);
 
 function updateTimeSlotItems() {
+    // // get current time in hours
+    var now = moment().hour();
 
-    // // apply hours through Moment.js (.hour class) and compare with current time
-    // var now = moment().hour();
+    // grab hour from HTML ID for moment.js
+    $(".time-block").each(function() {
+        var timeBlock = parseInt($(this)).toLocaleString("id").split("hour")[1];
+        console.log(timeBlock);
+        // compare "now" with timeBlock above
+    });
 
-    // $(".hour").each(function () {
-    //     var timeSlot = 
+};
+//     // // less than hours means .past
 
-    // // less than hours means past
-    //      if (timeSlot < now) {
+//     //    }  // equal to 0 is .present
 
+//     //    // greater than hours is .future
+//     //     else { (timeSlot > now) 
 
-    //    }  // equal to 0 is present
-    //    else if (timeSlot === now) {
+// function updatePlanner() {
 
-
-    //    }
-    //    // greater than hours is future
-    //     else { (timeSlot > now) 
-
-    //     }
-    // })
-
-}
-
-function updatePlanner() {
-
-// save button 'click'
-//    // get value of description
-// var text = 
-//    // get value of time-block
-// var time =
-//    // store date in time-block with description
-// localstorage.setItem(time, text);
-}
-
-updateTimeSlotItems();
+// // save button (.saveBtn) 'click'
+// //    // get value of description
+// // var text = 
+// //    // get value of time-block
+// // var time =
+// //    // store date in time-block with description
+// // localstorage.setItem(time, text);
