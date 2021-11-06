d01da38c8ef30ace4627b3d92a8702bd4fbe5a01
Brian <BMArsenault@gmail.com>
2021-11-05 23:24:08 -0700

    testing variables and if statements

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-//  display date/time to top of page
-var dateToday = moment().format("MMMM Do, YYYY");
-$("#currentDay").append(dateToday);
+// //  display date/time to top of page
+// var dateToday = moment().format("MMMM Do, YYYY");
+// $("#currentDay").append(dateToday);
 
-function updateTimeSlotItems() {
-    // // get current time in hours
-    var now = moment().hour();
+// // // get current time in hours
+// var now = moment().hour();
 
-    // grab hour from HTML ID for moment.js
-    $(".time-block").each(function() {
-        var timeBlock = parseInt($(this)).toLocaleString("id").split("hour")[1];
-        console.log(timeBlock);
-        // compare "now" with timeBlock above
-    });
+// // set up save button
+// var saveBtn = $('.saveBtn')
+// saveBtn.click(handleSave)
 
-};
-//     // // less than hours means .past
+    function updateTimeSlotItems() {
 
-//     //    }  // equal to 0 is .present
+        // grab hour integer from HTML ID for moment.js
+        $(".time-block").each(function() {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
 
-//     //    // greater than hours is .future
-//     //     else { (timeSlot > now) 
+            // compare "now" with timeBlock above
+            // if time has passed the block, set as "past"
+            if (now.isAfter(timeBlock))
+            {   
+                $(".description").addClass("past");
+            }
+            //  if time is on current block of time, set as "present"
+            else if (now === timeBlock)
+            {
+                $(".description").addClass("present");
+            }
+            //  if time hasn't reached block of time, set as "future"
+            else (now < timeBlock)
+            {
+                $('.description').addClass("future");
+            }
+        }
+        );
+    };
 
 // function updatePlanner() {
 
